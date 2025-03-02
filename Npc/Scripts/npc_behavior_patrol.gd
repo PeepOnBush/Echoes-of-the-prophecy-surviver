422034b30977737f6b4b0dc1792388a2abadbc7a
PeepOnBush <phandangloi07102014@gmail.com>
2025-03-03 00:28:07 +0700

    finished patrol system.  Attempt to migrating to 4.3 2/3/2025

--- Npc/Scripts/npc_behavior_patrol.gd
+++ Npc/Scripts/npc_behavior_patrol.gd
@@ -1,1 +1,88 @@
-extends Node
+@tool
+extends NPCBehavior
+
+const COLORS = [Color(1,0,0) , Color(1,1,0), Color(1,1,1), Color(0,1,0), Color(0,1,1), Color(0,0,1), Color(1,0,1)]
+
+@export var walk_speed : float = 30.0
+
+var patrol_location : Array[PatrolLocation]
+var current_location_index : int = 0
+var target : PatrolLocation
+
+func _ready() -> void:
+	gatherPatrolLocation()
+	if Engine.is_editor_hint():
+		child_entered_tree.connect(gatherPatrolLocation)
+		child_order_changed.connect(gatherPatrolLocation)
+		return
+	super()
+	if patrol_location.size() == 0:
+		process_mode = Node.PROCESS_MODE_DISABLED
+		return
+	target = patrol_location[0]
+
+func _process(_delta) -> void:
+	if Engine.is_editor_hint():
+		return
+	if npc.global_position.distance_to(target.target_position) < 1:
+		start()
+
+func start() -> void:
+	if npc.doBehavior == false or patrol_location.size() < 2:
+		return
+	
+	#IDLE PHASE
+	npc.global_position = target.target_position
+	npc.state = "idle"
+	npc.velocity = Vector2.ZERO
+	npc.updateAnimation()
+	
+	var wait_time : float = target.wait_time
+	current_location_index += 1
+	if current_location_index >= patrol_location.size():
+		current_location_index = 0
+	
+	target = patrol_location[current_location_index]
+	
+	await get_tree().create_timer(wait_time).timeout
+	
+	if npc.doBehavior == false:
+		return
+	
+	npc.state = "walk"
+	var _dir = global_position.direction_to(target.target_position)
+	npc.direction = _dir
+	npc.velocity = walk_speed * _dir
+	npc.updateDirection(target.target_position)
+	npc.updateAnimation()
+	pass
+
+func gatherPatrolLocation( _n : Node = null) -> void:
+	patrol_location = []
+	for c in get_children():
+		if c is PatrolLocation:
+			patrol_location.append(c)
+	if Engine.is_editor_hint():
+		if patrol_location.size() > 0:
+			for i in patrol_location.size():
+				var _p = patrol_location[i] as PatrolLocation
+				
+				if not _p.transform_changed.is_connected(gatherPatrolLocation):
+					_p.transform_changed.connect(gatherPatrolLocation)
+				
+				_p.updateLabel(str(i))
+				_p.modulate = getColorByIndex(i)
+				
+				var _next : PatrolLocation
+				if i < patrol_location.size() - 1:
+					_next = patrol_location[i+1]
+				else:
+					_next = patrol_location[0]
+				_p.updateLine(_next.position)
+	pass
+
+func getColorByIndex( i : int) -> Color:
+	var color_count : int = COLORS.size()
+	while i > color_count - 1:
+		i -= color_count
+	return COLORS[i]
---
+++ Npc/Scripts/patrol_location.gd
@@ -0,0 +1,40 @@
+@tool
+class_name PatrolLocation extends Node2D
+
+signal transform_changed
+
+@export var wait_time : float = 0.0:
+	set(v) :
+		wait_time = v 
+		updateWaitTimeLabel()
+
+var target_position : Vector2 = Vector2.ZERO
+
+func _enter_tree()-> void:
+	set_notify_transform(true)
+
+func _notification(what : int ) -> void:
+	if what == NOTIFICATION_TRANSFORM_CHANGED:
+		transform_changed.emit()
+
+func _ready() -> void:
+	target_position = global_position
+	updateWaitTimeLabel()
+	if Engine.is_editor_hint():
+		return
+	
+	$Sprite2D.queue_free()
+	pass
+func updateWaitTimeLabel() -> void:
+	if Engine.is_editor_hint():
+		$Sprite2D/Label2.text = "wait:" + str( snappedf(wait_time,0.01 )) + " secs"
+	pass
+
+func updateLabel( _s : String) -> void:
+	$Sprite2D/Label.text = _s
+	pass
+
+func updateLine( nextLocation : Vector2) -> void:
+	var line : Line2D = $Sprite2D/Line2D
+	line.points[1] = nextLocation - position
+
