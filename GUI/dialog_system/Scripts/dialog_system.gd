e2aa400a031172cb8a463532cce90091473871a7
PeepOnBush <phandangloi07102014@gmail.com>
2025-03-03 17:52:29 +0700

    GUI Dialog system script, scene established

---
+++ GUI/dialog_system/Scripts/dialog_system.gd
@@ -0,0 +1,13 @@
+@tool
+@icon("res://GUI/dialog_system/Icons/star_bubble.svg") 
+class_name DialogSystemNode extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
