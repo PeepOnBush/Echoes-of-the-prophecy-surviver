482070d8ffcc75addb568f7a18a932e396f3038d
PeepOnBush <phandangloi07102014@gmail.com>
2025-03-06 20:22:33 +0700

    finished dialog basic and advance system

---
+++ GUI/dialog_system/Scripts/dialog_potrait.gd
@@ -0,0 +1,68 @@
+@tool
+class_name DialogPotrait extends Sprite2D
+
+
+var blink : bool = false : set = setBlink
+var open_mouth : bool = false : set = setOpenMouth
+var mouth_open_frames : int = 0
+var audio_pitch_base : float = 1.0
+
+@onready var audio_stream_player: AudioStreamPlayer = $"../AudioStreamPlayer"
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	DialogSystem.letter_added.connect(checkMouthOpen)
+	blinker()
+	pass # Replace with function body.
+
+func checkMouthOpen( l : String) -> void:
+	if 'aeiouy1234567890'.contains(l):
+		open_mouth = true
+		mouth_open_frames += 3
+		audio_stream_player.pitch_scale = randf_range(audio_pitch_base - 0.04 ,audio_pitch_base + 0.04 )
+		audio_stream_player.play()
+	elif '.,!?'.contains(l):
+		mouth_open_frames = 0
+	
+	if mouth_open_frames > 0:
+		mouth_open_frames -= 1
+	
+	if mouth_open_frames == 0:
+		open_mouth = false
+	pass
+
+func updatePotrait() -> void:
+	if open_mouth == true:
+		frame = 2
+	else : 
+		frame = 0
+	
+	if blink == true:
+		frame += 1
+
+func setBlink(_value : bool) -> void:
+	if blink != _value:
+		blink = _value
+		updatePotrait()
+	pass
+
+func blinker() -> void:
+	if blink == false:
+		await get_tree().create_timer(randf_range(0.1,3)).timeout
+	else:
+		await get_tree().create_timer(0.15).timeout
+	blink = not blink
+	blinker()
+
+func setOpenMouth(_value : bool) -> void:
+	if open_mouth != _value:
+		open_mouth = _value
+		updatePotrait()
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- GUI/dialog_system/Scripts/dialog_system.gd
+++ GUI/dialog_system/Scripts/dialog_system.gd
@@ -3,17 +3,27 @@
 class_name DialogSystemNode extends CanvasLayer
 
 signal finished
+signal letter_added(letter :String)
+
 
 var isActive : bool = false
+var textInProgress : bool = false
+
+var text_speed : float = 0.02
+var text_length : int = 0
+var plain_text : String
+
 var dialog_items : Array[DialogItem]
 var dialog_item_index : int = 0
 
 @onready var dialog_ui: Control = $DialogUi
 @onready var content : RichTextLabel = $DialogUi/PanelContainer/RichTextLabel
 @onready var name_label: Label = $DialogUi/NameLabel
-@onready var potrait_sprite: Sprite2D = $DialogUi/PotraitSprite
+@onready var potrait_sprite: DialogPotrait = $DialogUi/PotraitSprite
 @onready var dialog_progress_indicator: PanelContainer = $DialogUi/DialogProgressIndicator
 @onready var dialog_progress_indicator_label: Label = $DialogUi/DialogProgressIndicator/Label
+@onready var audio_stream_player: AudioStreamPlayer = $DialogUi/AudioStreamPlayer
+@onready var timer: Timer = $DialogUi/Timer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -23,6 +33,7 @@ func _ready() -> void:
 			get_parent().remove_child(self)
 			return
 		return
+	timer.timeout.connect( onTimerTimeout)
 	hideDialog()
 	pass
 
@@ -35,6 +46,12 @@ func _unhandled_input(event: InputEvent) -> void:
 		event.is_action_pressed("attack") or
 		event.is_action_pressed("ui_accept")
 	):
+		if textInProgress == true :
+			content.visible_characters = text_length
+			timer.stop()
+			textInProgress = false
+			showDialogButtonIndicator(true)
+			return
 		dialog_item_index +=1
 		if dialog_item_index < dialog_items.size():
 			startDialog()
@@ -60,9 +77,31 @@ func hideDialog() -> void:
 	pass
 
 func startDialog() -> void:
-	showDialogButtonIndicator(true)
+	showDialogButtonIndicator(false)
 	var _d : DialogItem = dialog_items[dialog_item_index]
 	setDialogData(_d)
+	
+	content.visible_characters = 0
+	text_length = content.get_total_character_count()
+	plain_text = content.get_parsed_text()
+	textInProgress = true
+	startTimer()
+	pass
+
+func startTimer() -> void:
+	timer.wait_time = text_speed
+	#manipulate wait_time
+	timer.start()
+	pass
+
+func onTimerTimeout() -> void:
+	content.visible_characters +=1
+	if content.visible_characters <= text_length:
+		letter_added.emit(plain_text[content.visible_characters - 1])
+		startTimer()
+	else:
+		showDialogButtonIndicator(true)
+		textInProgress = false
 	pass
 
 func showDialogButtonIndicator( _isVisible : bool ) -> void:
@@ -78,4 +117,5 @@ func setDialogData( _d : DialogItem) -> void:
 		content.text = _d.text
 	name_label.text = _d.npc_info.npc_name
 	potrait_sprite.texture = _d.npc_info.portrait
+	potrait_sprite.audio_pitch_base = _d.npc_info.dialog_audio_pitch
 	pass
