b2e4a5663ff0b69ee40e7e6ceb80011dc92c53de
PeepOnBush <phandangloi07102014@gmail.com>
2025-03-08 15:30:48 +0700

    Add some node for the dialog branches and choices, add the GUI for the choices ( maybe a lil more fix later in the future)

---
+++ Interactables/Dialog/Scripts/dialog_branch.gd
@@ -0,0 +1,21 @@
+@tool
+@icon("res://GUI/dialog_system/Icons/answer_bubble.svg")
+class_name DialogBranch extends DialogItem
+
+@export var text : String = "ok...."
+
+var dialog_items : Array[DialogItem]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	
+	for c in get_children():
+		if c is DialogItem:
+			dialog_items.append(c)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- Interactables/Dialog/Scripts/dialog_choice.gd
+++ Interactables/Dialog/Scripts/dialog_choice.gd
@@ -3,13 +3,25 @@
 
 class_name DialogChoice extends DialogItem
 
-@export var choices : Array[String] = ["yes" ,"no"]
-
+var dialog_branches : Array[DialogBranch]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+func _get_configuration_warnings() -> PackedStringArray:
+	if checkForDialogBranches() == false:
+		return ["Requires atleast 2 DialogBranches nodes."]
+	else:
+		return []
 
+func checkForDialogBranches() -> bool:
+	var _count : int = 0
+	for c in get_children():
+		if c is DialogBranch:
+			_count += 1
+			if _count > 1:
+				return true
+	return false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
