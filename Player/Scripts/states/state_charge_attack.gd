9c9b93fbdf48a50ca5d42a5042d0cff6d99423d0
PeepOnBush <phandangloi07102014@gmail.com>
2025-03-15 14:23:49 +0700

    Completed skills system (kinda)

--- Player/Scripts/states/state_charge_attack.gd
+++ Player/Scripts/states/state_charge_attack.gd
@@ -7,6 +7,12 @@ class_name StateChargeAttack extends State
 
 
 @onready var idle: stateIdle = $"../idle"
+@onready var charge_hurt_box: HurtBox = %ChargeHurtBox
+@onready var charge_spin_hurt_box: HurtBox = %ChargeSpinHurtBox
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $"../../Audio/AudioStreamPlayer2D"
+@onready var spin_animation_player: AnimationPlayer = $"../../Sprite2D/SpinEffectSprite2D/AnimationPlayer"
+@onready var spin_effect_sprite_2d: Sprite2D = $"../../Sprite2D/SpinEffectSprite2D"
+@onready var gpu_particles_2d: GPUParticles2D = $"../../Sprite2D/ChargeHurtBox/GPUParticles2D"
 
 var timer : float = 0.0
 var walking : bool = false
@@ -19,6 +25,9 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func init() -> void:
+	gpu_particles_2d.emitting = false
+	particles = gpu_particles_2d.process_material as ParticleProcessMaterial
+	spin_effect_sprite_2d.visible = false
 	pass
 
 ## What happen when the player enter this state ?
@@ -26,10 +35,19 @@ func Enter() -> void:
 	timer = charge_duration
 	is_attacking = false
 	walking = false
+	charge_hurt_box.monitoring = true
+	gpu_particles_2d.emitting = true
+	gpu_particles_2d.amount = 4
+	gpu_particles_2d.explosiveness = 0
+	particles.initial_velocity_min = 10
+	particles.initial_velocity_max = 30
 	pass
 ## What happen when the player exit this state ?
 func Exit() -> void:
-	
+	charge_hurt_box.monitoring = false
+	charge_spin_hurt_box.monitoring = false
+	spin_effect_sprite_2d.visible = false
+	gpu_particles_2d.emitting = false
 	pass 
 ## What happen when the _process update in this state ?
 func Process(_delta : float) -> State:
@@ -38,6 +56,7 @@ func Process(_delta : float) -> State:
 		timer -= _delta
 		if timer <= 0:
 			timer = 0  
+			chargeComplete()
 	
 	
 	if is_attacking == false:
@@ -51,7 +70,19 @@ func Process(_delta : float) -> State:
 	player.velocity = player.direction * move_speed
 	
 	return null
-	
+
+func chargeComplete()-> void:
+	playAudio(sfx_charged)
+	gpu_particles_2d.amount = 50
+	gpu_particles_2d.explosiveness = 1
+	particles.initial_velocity_min = 50
+	particles.initial_velocity_max = 100
+	await get_tree().create_timer(0.5).timeout
+	gpu_particles_2d.amount = 10
+	gpu_particles_2d.explosiveness = 0
+	particles.initial_velocity_min = 10
+	particles.initial_velocity_max = 30
+
 func Physics(_delta : float) -> State:
 	return null
 
@@ -67,8 +98,12 @@ func chargeAttack() -> void:
 	is_attacking = true
 	player.animationPlayer.play("charge_attack")
 	player.animationPlayer.seek(getSpinFrame())
+	playAudio(sfx_spin)
+	spin_effect_sprite_2d.visible = true
+	spin_animation_player.play("spin")
 	var _duration : float = player.animationPlayer.current_animation_length
 	player.make_invulnerable( _duration )
+	charge_spin_hurt_box.monitoring = true
 	await get_tree().create_timer(_duration * 0.875).timeout
 	
 	state_machine.changeState(idle)
@@ -83,3 +118,8 @@ func getSpinFrame() -> float:
 			return interval * 4
 		_:
 			return interval * 6
+
+func playAudio(_audio : AudioStream) -> void:
+	audio_stream_player_2d.stream = _audio
+	audio_stream_player_2d.play()
+	pass
