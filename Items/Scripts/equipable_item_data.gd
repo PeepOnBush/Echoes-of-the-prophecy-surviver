95f08b256c6036e67fd0509ae1cec83949f5eab5
PeepOnBush <phandangloi07102014@gmail.com>
2025-05-27 15:29:06 +0700

    Finished the equipment texture changed and changes the name

--- Items/Scripts/equipable_item_data.gd
+++ Items/Scripts/equipable_item_data.gd
@@ -3,3 +3,4 @@ class_name EquipableItemData extends ItemData
 enum Type {WEAPON, ARMOR, AMULET, RING, TOOL, SHIELD }
 @export var type : Type = Type.WEAPON
 @export var modifiers : Array[EquipableItemModifier] 
+@export var sprite_texture : Texture
