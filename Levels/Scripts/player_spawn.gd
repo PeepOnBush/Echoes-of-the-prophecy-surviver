9e10e231210b002eead19e4d6432a06f536d0bdd
PeepOnBush <phandangloi07102014@gmail.com>
2026-03-13 23:25:38 +0700

    13/03/2026 insert new items to enemies

--- Levels/Scripts/player_spawn.gd
+++ Levels/Scripts/player_spawn.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false 
-	# If the LevelManager says THIS is the target node, spawn the player here!
+	
+	print("--- SPAWN CHECK ---")
+	print("My Name is: ", self.name)
+	print("LevelManager is looking for: ", LevelManager.target_transition)
+	
 	if self.name == LevelManager.target_transition:
-		PlayerManager.set_player_position(global_position)
-		pass
-	# Fallback for the very first time the game boots up
+		# THE FIX: Wait one physics frame so global_position calculates correctly
+		call_deferred("teleport_player")
+		
 	elif PlayerManager.playerSpawned == false:
-		PlayerManager.set_player_position(global_position)
+		call_deferred("teleport_player")
 		PlayerManager.playerSpawned = true
-		pass
-	pass
+	else:
+		print("NO MATCH. Ignoring this spawn point.")
+
+func teleport_player() -> void:
+	print("Teleporting Player to TRUE position: ", global_position)
+	PlayerManager.set_player_position(global_position)
